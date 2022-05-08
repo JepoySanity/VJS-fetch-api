af99a17229b2a7ff29c0b20961e69027481ef0f9
Jepoy Factoran <factoran20@gmail.com>
2022-05-08 13:05:05 +0800

    fetch to github users api

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 document.getElementById("get-text").addEventListener("click", getText);
 document.getElementById("get-json").addEventListener("click", getJson);
+document.getElementById("get-api").addEventListener("click", getAPI);
 
 function getText() {
   fetch("text.txt")
@@ -27,6 +28,28 @@ function getJson() {
             <li>${post.title}</li>
             <li>${post.post}</li>
           </ul>
+        `;
+      });
+      document.getElementById("output").innerHTML = output;
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function getAPI() {
+  fetch("https://api.github.com/users")
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      let output = "";
+      data.forEach(function (user) {
+        output += `
+          <div>
+            <label for="user">${user.login}</label>
+            <img src="${user.avatar_url}" width="100" height="100">
+          </div>
         `;
       });
       document.getElementById("output").innerHTML = output;
