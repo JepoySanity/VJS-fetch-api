ba7c2ef018cd0b116e18e48b7a8f46e74041f55c
Jepoy Factoran <factoran20@gmail.com>
2022-05-08 12:57:48 +0800

    add json file and create getJson functionality

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 document.getElementById("get-text").addEventListener("click", getText);
+document.getElementById("get-json").addEventListener("click", getJson);
 
 function getText() {
-  fetch("texts.txt")
+  fetch("text.txt")
     .then(function (res) {
       return res.text();
     })
     .then(function (data) {
-      console.log(data);
+      document.getElementById("output").innerHTML = data;
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function getJson() {
+  fetch("post.json")
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      let output = "";
+      data.forEach(function (post) {
+        output += `
+          <ul>
+            <li>${post.title}</li>
+            <li>${post.post}</li>
+          </ul>
+        `;
+      });
+      document.getElementById("output").innerHTML = output;
     })
     .catch(function (err) {
       console.log(err);
